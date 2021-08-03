9230e01747718fb81b4203bb18d2dac8b88ee872
John <jpa.khalife@gmail.com>
2021-08-02 22:39:13 -0400

    Spelling error fixed

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ client.on('message',message => {
     }else if (command === 'reactionrole') {
         client.commands.get('reactionrole').execute(message, args, Discord, client);
     } else if (command === 'account') {
-        client.commands.get('accounts').execute(message, args, Discord, client);
+        client.commands.get('account').execute(message, args, Discord, client);
     } 
 
 });
