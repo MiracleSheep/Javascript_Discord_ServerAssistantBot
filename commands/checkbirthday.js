6a1ca4f262a46caffbfbec6231369d0de15637da
John Khalife <jpa.khalife@gmail.com>
2021-12-11 23:00:48 -0500

    I am struggling with vraiable types

--- commands/checkbirthday.js
+++ commands/checkbirthday.js
@@ -58,9 +58,9 @@ module.exports = {
                       if (err) {
                           message.channel.send('There was an error getting the birthday.')
                       } else {
-                          console.log(result[0].toString())
+                          console.log(result)
                           try {
-                            message.channel.send('The birthday of the person you specified is ' + result[0].toString())
+                            message.channel.send('The birthday of the person you specified is ' + result)
                           } catch (error) {
                             message.channel.send('There was an error getting the birthday.')
                             console.log(error)
