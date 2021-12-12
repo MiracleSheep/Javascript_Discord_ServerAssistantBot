8e10ef80f6007d3edd11c402925b892308645489
John Khalife <jpa.khalife@gmail.com>
2021-12-11 22:44:20 -0500

    added a tostring to try and display the date

--- commands/checkbirthday.js
+++ commands/checkbirthday.js
@@ -58,9 +58,9 @@ module.exports = {
                       if (err) {
                           message.channel.send('There was an error getting the birthday.')
                       } else {
-                          console.log(result[0].date)
+                          console.log(result[0].date.toString())
                           try {
-                            message.channel.send('The birthday of the person you specified is ' + result.date)
+                            message.channel.send('The birthday of the person you specified is ' + result[0].date.toString())
                           } catch (error) {
                             message.channel.send('There was an error getting the birthday.')
                             console.log(error)
