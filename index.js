8542ee846e6db8065997fcb3840d1b26d40cd485
John Khalife <jpa.khalife@gmail.com>
2021-12-13 20:33:02 -0500

    test

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ client.once('ready', () => {
 });
 
 //This is the cron function that checks birthdays everyday
-const job = schedule.scheduleJob('34 20 * * *', function() {
+const job = schedule.scheduleJob('35 20 * * *', function() {
     console.log("Time functuion called")
     check()
 })
